75badedf31b7af3fd96535dc6fa5c18043340490
Przemyslaw Bykowski <p.bykowski@sdacademy.pl>
2019-06-06 05:23:06 +0200

    added hello cd api

---
+++ src/main/java/pl/bykowski/javacoursesapi/HelloApi.java
@@ -0,0 +1,13 @@
+package pl.bykowski.javacoursesapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloApi {
+
+    @GetMapping("/hello")
+    public String get() {
+        return "hello CD";
+    }
+}
